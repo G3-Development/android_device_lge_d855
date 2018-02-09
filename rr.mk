c23fa5a943c29b008d6e627864c0d9151444de16
Mountaser Halak <mountaser1998@gmail.com>
2018-02-09 20:13:09 +0100

    rrfying

--- rr.mk
+++ rr.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := rr_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
