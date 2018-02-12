b35de4bc32b4d241c95f78ef814b201e5a6829f2
RMarchetto96 <rafagossmann@gmail.com>
2018-02-12 19:22:56 +0530

    Let's do this simple as possible.

--- rr.mk
+++ rr.mk
@@ -1,9 +1,17 @@
-$(call inherit-product, device/lge/d855/full_d855.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from d855 device
+$(call inherit-product, device/lge/d855/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_NAME := rr_d855
+PRODUCT_DEVICE := d855
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-D855
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
